1dad8b803e11aa5fcaa3ebe78ff3afe711b0e750
IsacBianor <isacbianor@gmail.com>
2021-06-14 08:35:12 -0300

    Implementação principal no delete-produto e somos

--- src/app/produto/produto.component.ts
+++ src/app/produto/produto.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Categoria } from '../model/Categoria';
 import { Produto } from '../model/Produto';
 import { Usuario } from '../model/Usuario';
 import { AlertasService } from '../service/alert.service';
+import { AuthService } from '../service/auth.service';
+import { CategoriaService } from '../service/categoria.service';
+import { ProdutoService } from '../service/produto.service';
 
 @Component({
   selector: 'app-produto',
@@ -15,9 +19,19 @@ export class ProdutoComponent implements OnInit {
   produto: Produto = new Produto()
   usuario: Usuario = new Usuario()
 
+  idUsuario: number = environment.id
+
+  categoria: Categoria = new Categoria()
+  idCategoria: number
+  listaCategoria: Categoria[]
+
   constructor(
     private router:Router,
+    private route: ActivatedRoute,
     private alertas:AlertasService,
+    private produtoService: ProdutoService,
+    private categoriaService:CategoriaService,
+    private usuarioService: AuthService
   ) { }
 
   ngOnInit( ){
@@ -26,9 +40,46 @@ export class ProdutoComponent implements OnInit {
       this.alertas.showAlertDanger('Sua sessão expirou,faça o login novamente')
       this.router.navigate(['/entrar'])
     }
+
+    let id = this.route.snapshot.params['id']
+    this.findByIdProduto (id)
+    this.findByIdUsuario (this.idUsuario)
   }
 
-  publicar(){
+  findByIdProduto(id:number){
+    this.produtoService.getByIdProduto(id).subscribe((resp: Produto)=>{
+      this.produto = resp
+    })
+  }
+
+  findByIdCategoria(){
+    this.categoriaService.getByIdCategoria(this.idCategoria).subscribe((resp:Categoria)=>{
+      this.categoria = resp
+    })
+  }
+
+  findByIdUsuario(idUsuario: number){
+    this.usuarioService.getByIdUser(this.idUsuario).subscribe((resp:Usuario)=>{
+      this.usuario = resp
+    })
+  }
+
+  findAllCategoria(){
+    this.categoriaService.getAllCategoria().subscribe((resp: Categoria[])=>{
+      this.listaCategoria = resp
+    })
+  }
+
+  editar(){
+    this.categoria.id = this.idCategoria
+    this.produto.categoria = this.categoria
+
+    this.produtoService.putProduto(this.produto).subscribe((resp: Produto)=>{
+      this.produto = resp
+
+      this.alertas.showAlertSuccess('Produto atualizado com sucesso!')
+      this.router.navigate([''])
+    })
 
   }
   
